0759ad241a73567e0af2f2774c13cc99f81ac8e4
baseer451 <35836315+baseer451@users.noreply.github.com>
2020-06-29 11:07:19 +0500

    added firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,50 @@
-#include <FirebaseArduino.h>
+#include <JC_Button.h>
 #include <ESP8266WiFi.h>
-
-// Set these to run example.
-#define WIFI_SSID "Shoaib"
+#include <FirebaseArduino.h>
+#define FIREBASE_HOST "led1-b7e49.firebaseio.com"
+#define FIREBASE_AUTH "rQjkcrr0KciU1Opt9CaKlPQto0byD2qZNqPyUuvi"
+#define WIFI_SSID "Shoaib" //your WiFi SSID for which yout NodeMCU connects
 #define WIFI_PASSWORD "A0123456789"
 
-
-void setup() {
-  Serial.begin(9600);
-
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
-  
-  Firebase.begin("publicdata-cryptocurrency.firebaseio.com");
-  Firebase.stream("/bitcoin/last");  
+int val1;
+int last_val;
+Button mybtn(4);
+int relay = 13;
+void setup()
+{
+    mybtn.begin();
+    Serial.begin(9600);
+    pinMode(relay, OUTPUT);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("connected:");
+    Serial.println(WiFi.localIP());
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Firebase.setInt("light1",1); //Here the varialbe"S1","S2","S3" and "S4" needs to be the one which is used in our Firebase and MIT App Inventor
+    digitalWrite(relay, LOW);
 }
 
-
-void loop() {
-  if (Firebase.failed()) {
-    Serial.println("streaming error");
-    Serial.println(Firebase.error());
+void loop()
+{
+    mybtn.read();
+    val1 = Firebase.getString("S1").toInt();
+    last_val;
+    /* if (mybtn.wasReleased() || val1 == 1)
+  {
+    digitalWrite(relay, !digitalRead(relay));
+    Firebase.set("light1", 1);
+    val1 = !val1;
+    Serial.println(val1);
   }
-  
-  if (Firebase.available()) {
-     FirebaseObject event = Firebase.readEvent();
-     String eventType = event.getString("type");
-     eventType.toLowerCase();
-     
-     Serial.print("event: ");
-     Serial.println(eventType);
-     if (eventType == "put") {
-       Serial.print("data: ");
-       Serial.println(event.getString("data"));
-       String path = event.getString("path");
-       String data = event.getString("data");
-
-      
-       Serial.println(data);
-      
-     }
-  }   
+    */
+    if (val1 == 1)
+    {
+        digitalWrite(relay, HIGH);
+    }
 }
